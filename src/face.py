66d5e6c02f82c26540c80c65aea35e9411dd6024
Dhruv Matani <dhruvbird@gmail.com>
2010-10-29 22:01:19 +0000

    src/face.py: Added email ID

--- src/face.py
+++ src/face.py
@@ -4,6 +4,7 @@ An efficient auto-complete library suitable for large volumes of data.
 
 
 Copyright (c) 2010, Dhruv Matani
+dhruvbird@gmail.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
