c0af55c00aef2546fe7c7f5ce29ba4d1b3c4fde5
Dhruv Matani <dhruvbird@gmail.com>
2011-10-24 12:16:35 +0000

    Add a timed test case for edit_distance

--- include/editdistance.hpp
+++ include/editdistance.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <assert.h>
 #include <utility>
+#include <time.h>
 
 int
 edit_distance(std::string const& lhs, std::string const& rhs) {
@@ -76,6 +77,18 @@ namespace editdistance {
         cerr<<"Edit Distance("<<s1<<", "<<s2<<"): "<<edit_distance(s1, s2)<<endl;
         assert(edit_distance(s1, s2) == 8);
 
+        s1 = "abracadabra magic! magic! magic!"; s2 = "the great Hoodini";
+        int x = 0;
+        clock_t start = clock();
+        const int ntimes = 50000;
+        for (int i = 0; i < ntimes; ++i) {
+            x = edit_distance(s1, s2);
+        }
+        clock_t end = clock();
+        double sec_diff = (double)(end - start) / CLOCKS_PER_SEC;
+
+        cerr<<"Edit Distance("<<s1<<", "<<s2<<"): "<<x<<endl;
+        cerr<<"Time to compute "<<ntimes<<" times: "<<sec_diff<<" second. sec/compare: "<<sec_diff/ntimes<<endl;
         return 0;
     }
 }
