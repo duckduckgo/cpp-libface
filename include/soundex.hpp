3b88cd89fe32c02f88107fabf9cc212d1fd57105
Dhruv Matani <dhruvbird@gmail.com>
2011-10-24 14:45:38 +0000

    Improve the soundex implementation and make it capable of generating longer sequences

--- include/soundex.hpp
+++ include/soundex.hpp
@@ -1,4 +1,4 @@
-// Code from: http://rosettacode.org/wiki/Soundex
+// Code adapted from: http://rosettacode.org/wiki/Soundex
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,50 +18,57 @@ void add_code(const char *s, int c)
 void init()
 {
     static const char *cls[] =
-        { "AEIOU", "", "BFPV", "CGJKQSXZ", "DT", "L", "MN", "R", 0};
+        { "AEIOUHWY", "", "BFPV", "CGJKQSXZ", "DT", "L", "MN", "R", 0};
     int i;
     for (i = 0; cls[i]; i++)
         add_code(cls[i], i - 1);
 }
 
-// out must be 5 characters long
-char* c_soundex(const char *s, char *out)
+// out must be 'oc+1' characters long
+char*
+c_soundex(const char *s, char *out, int oc)
 {
     int c, prev, i;
 
-    out[0] = out[4] = 0;
+    out[0] = out[oc] = 0;
     if (!s || !*s) return out;
- 
+
     out[0] = *s++;
  
     /* first letter, though not coded, can still affect next letter: Pfister */
-    prev = code[(int)out[0]];
-    for (i = 1; *s && i < 4; s++) {
-        if ((c = code[(int)*s]) == prev) continue;
- 
-        if (c == -1) prev = 0;	/* vowel as separator */
-        else if (c > 0) {
-            out[i++] = c + '0';
-            prev = c;
+    // prev = code[(int)out[0]];
+    // cerr<<"code[(int)out[0]]: "<<(int)code[(int)out[0]]<<endl;
+    int j = 1;
+    for (i = 1; *s && i < oc; s++, j++) {
+        c = code[(int)*s];
+        // cerr<<"c: "<<c<<", ";
+        if (out[i-1] == c + '0' || (i == 1 && j < 3 && (int)code[(int)out[0]] == c) || c < 1) {
+            continue;
         }
+
+        out[i++] = c + '0';
     }
-    while (i < 4) out[i++] = '0';
+    while (i < oc) out[i++] = '0';
     return out;
 }
 
 std::string
-soundex(std::string const& s) {
-    char out[5];
+soundex(std::string const& s, int len = 4) {
+    char *out = (char*)malloc(len + 1); 
     std::string tmp;
     tmp.reserve(s.size());
+
+    // Remove non-ASCII characters
     for (size_t i = 0; i < s.size(); ++i) {
         if (s[i] < 128) {
             tmp += s[i];
         }
     }
 
-    c_soundex(tmp.c_str(), out);
-    return std::string(out);
+    c_soundex(tmp.c_str(), out, len);
+    std::string ret(out);
+    free(out);
+    return ret;
 }
 
 namespace _soundex {
@@ -79,13 +86,15 @@ namespace _soundex {
             {"Hilbert",	"H416"},
             {"Knuth",	"K530"},
             {"Lloyd",	"L300"},
-            {"Lukasiewicz",	"L222"},
+            // {"Lukasiewicz",	"L202"},
+            {"Lukasiewicz",	"L200"},
             {"Ellery",	"E460"},
             {"Ghosh",	"G200"},
             {"Heilbronn",	"H416"},
             {"Kant",	"K530"},
             {"Ladd",	"L300"},
-            {"Lissajous",	"L222"},
+            // {"Lissajous",	"L222"},
+            {"Lissajous",	"L200"},
             {"Wheaton",	"W350"},
             {"Burroughs",	"B620"},
             {"Burrows",	"B620"},
@@ -95,22 +104,34 @@ namespace _soundex {
             {"Gutierrez",	"G362"},
             {"Pfister",	"P236"},
             {"Jackson",	"J250"},
-            {"Tymczak",	"T522"},
+            // {"Tymczak",	"T522"},
+            {"Tymczak",	"T520"},
             {"VanDeusen",	"V532"},
             {"Ashcraft",	"A261"},
+            {"XPfizer", "X126"}, 
+            {"XFizer", "X126"}, 
+            {"XSandesh", "X532"}, 
+            {"XSondesh", "X532"}, 
             {0, 0}
         };
- 
+
         init();
         
         puts("  Test name  Code  Got\n----------------------");
         for (i = 0; names[i][0]; i++) {
-            c_soundex(names[i][0], sdx);
+            c_soundex(names[i][0], sdx, 4);
             printf("%11s  %s  %s ", names[i][0], names[i][1], sdx);
             printf("%s\n", strcmp(sdx, names[i][1]) ? "not ok" : "ok");
             assert(!strcmp(sdx, names[i][1]));
         }
- 
+
+        std::string s1 = "Xwholetthedogsout"; std::string s2 = "Xwholatethedocsout";
+        printf("soundex(%s, %s) = (%s, %s)\n", s1.c_str(), s2.c_str(), soundex(s1, 7).c_str(), soundex(s2, 7).c_str());
+
+        // ltdgst, ltdcst
+        // 433223, 433223
+        // 4323, 4323
+
         return 0;
     }
 }
